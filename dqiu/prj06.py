9eb7b45501aaa799cad508a6795b5c42f67be1a3
DomChiu <godspeed1024@hotmail.com>
2023-12-07 15:25:01 +0800

    Fix the wrong version prj06.py

--- dqiu/prj06.py
+++ dqiu/prj06.py
@@ -1,7 +1,3 @@
-import easynn as nn
-import easynn_golden as golden
-import easynn_cpp as cpp
-
 import numpy as np
 import time
 
@@ -46,9 +42,7 @@ def Conv2d(in_nchw, kernel_W, kernel_b):
     if C != IC or kernel_b.shape != (OC,):
         raise Exception("Conv2d size mismatch: %s @ (%s, %s)" % (
             in_nchw.shape, kernel_W.shape, kernel_b.shape))
-    # print("Conv2d(N, H, W, C, OC, KH, KW)=(%d, %d, %d, %d, %d, %d, %d)" % (N, H, W, C, OC, KH, KW))
-    # print("kernel_W.shape: %s" % str(kernel_W.shape))
-    # print("kernel_b.shape: %s" % str(kernel_b.shape))
+
     # view in_nchw as a 6D tensor
     shape = (N, IC, H-KH+1, W-KW+1, KH, KW)
     strides = in_nchw.strides+in_nchw.strides[2:]
@@ -57,23 +51,6 @@ def Conv2d(in_nchw, kernel_W, kernel_b):
     # np.einsum("nihwyx,oiyx->nohw", data, kernel_W)
     nhwo = np.tensordot(data, kernel_W, ((1,4,5), (1,2,3)))
     return nhwo.transpose(0,3,1,2)+kernel_b.reshape((1, OC, 1, 1))
-
-def Conv2d_cpp(in_nchw, kernel_W, kernel_b):
-    OC, IC, KH, KW = kernel_W.shape
-    N, C, H, W = in_nchw.shape
-    if C != IC or kernel_b.shape != (OC,):
-        raise Exception("Conv2d size mismatch: %s @ (%s, %s)" % (
-            in_nchw.shape, kernel_W.shape, kernel_b.shape))
-
-    conv2d = nn.Conv2d("c", IC, OC, KH)
-    expr = conv2d(nn.Input2d("x", H, W, C))
-    expr.resolve({
-        "c.weight": kernel_W,
-        "c.bias": kernel_b
-    })
-    eval = expr.compile(golden.Builder())
-    # e1 = p.compile(cpp.Builder())
-    return eval(x = in_nchw)
 
 # return p_b for the whole batch
 def Conv2d_backprop_b(p_out, in_nchw, kernel_W, kernel_b):
@@ -164,7 +141,7 @@ def forward(images, theta):
     x = images.astype(float).transpose(0,3,1,2)
 
     # cx = Conv2d_c1(x) ## N * 8 * 24*24
-    cx = Conv2d_cpp(x, c1_W, c1_b)
+    cx = Conv2d(x, c1_W, c1_b)
 
     # rx = ReLU(cx) ## N * 8 * 24*24
     rx = cx*(cx > 0)
@@ -173,7 +150,7 @@ def forward(images, theta):
     y = MaxPool_2by2(rx)
 
     # cy = Conv2d_c2(y) ## N * 8 * 8*8
-    cy = Conv2d_cpp(y, c2_W, c2_b)
+    cy = Conv2d(y, c2_W, c2_b)
 
     # ry = ReLU(cy) ## N * 8 * 8*8
     ry = cy*(cy > 0)
@@ -332,127 +309,3 @@ for iBound in range(len(Bounds)): # initial weight range
 print("The best hyper parameters are: (batch_size, bound, epsilon)=(%d, %f, %f); Test accuracy is: %f" % (batch_size, bound, epsilon, bestAcc))
 # save the weights to be submitted
 save_theta(bestTheta)
-
-### Plot 3D bar charts ###
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
-# from matplotlib.ticker import LinearLocator
-# import matplotlib.ticker as mticker
-# from matplotlib import style
-# style.use('classic')
-
-# OffsetAcc = 0.0
-# ZLimMin = 0.0
-# ZLimMax = 1.0
-
-    
-# ## https://stackoverflow.com/questions/3909794/plotting-mplot3d-axes3d-xyz-surface-plot-with-log-scale
-# def bounds_tick_formatter(val, pos=None):
-#     return str(bounds[int(val)])
-
-# def epsilon_tick_formatter(val, pos=None):
-#     return str(epsilons[int(val)])
-
-# def batchSize_tick_formatter(val, pos=None):
-#     return str(batchSizes[int(val)])
-
-# def accuracy_tick_formatter(val, pos=None):
-#     return "%0.1f" % (val + OffsetAcc)
-
-# bounds = np.sort(Bounds)
-# epsilons = np.sort(Epsilons)
-# batchSizes = np.sort(BatchSizes)
-
-# # setup the figure and axes
-# widthEpsilon = widthBound = 1.0
-# widthBatchSize = 1.0
-
-# _x = np.arange(len(bounds))
-# _y = np.arange(len(epsilons))
-# _xx, _yy = np.meshgrid(_x, _y)
-# x, y = _xx.ravel(), _yy.ravel()
-# iBounds = [np.where(Bounds == bounds[x[i]])[0][0] for i in range(len(x))]
-# iEpsilons = [np.where(Epsilons == epsilons[y[i]])[0][0] for i in range(len(y))]
-# accuracies = results[iBounds, iEpsilons]
-# accuracies = np.max(accuracies, axis=1)
-# top = accuracies[:, 0]
-# imax = np.argmax(top)
-# print("The best accuracy=%.4f at (bound, epsilon)=(%f, %f)" % (top[imax], bounds[x[imax]], epsilons[y[imax]]))
-# top -= OffsetAcc
-# bottom = np.zeros_like(top)
-# print(top)
-# print(imax)
-
-# fig = plt.figure(figsize=(8, 8))
-# ax1 = fig.add_subplot(111, projection='3d')
-# ax1.bar3d(x, y, bottom, widthBound, widthEpsilon, top, shade=True, color='w')
-# ax1.xaxis.set_major_formatter(mticker.FuncFormatter(bounds_tick_formatter))
-# ax1.yaxis.set_major_formatter(mticker.FuncFormatter(epsilon_tick_formatter))
-# ax1.zaxis.set_major_formatter(mticker.FuncFormatter(accuracy_tick_formatter))
-# ax1.set_xticks(_x)
-# ax1.set_yticks(_y)
-# ax1.set_xlabel("bound")
-# ax1.set_ylabel("epsilon")
-# ax1.set_zlabel("Accuracy")
-# ax1.set_zlim(ZLimMin, ZLimMax)
-# ax1.set_title('$(bound, \epsilon)$ vs. Accuracy')
-# # ax1.text(int(x[imax]) + 0.5, y[imax], top[imax]+0.2, "Text", color='#660066', backgroundcolor= '#c187e1', weight='bold', rotation='vertical', fontsize=10, horizontalalignment='left', verticalalignment='bottom')
-# ax1.text2D((x[imax] + 0.5)/len(_x), (y[imax] + 0.5)/len(_y), "2D Text", transform=ax1.transAxes)
-# ###
-# _x = np.arange(len(bounds))
-# _y = np.arange(len(batchSizes))
-# _xx, _yy = np.meshgrid(_x, _y)
-# x, y = _xx.ravel(), _yy.ravel()
-# iBounds = [np.where(Bounds == bounds[x[i]])[0][0] for i in range(len(x))]
-# iBatchSizes = [np.where(BatchSizes == batchSizes[y[i]])[0][0] for i in range(len(y))]
-# accuracies = results[iBounds, :, iBatchSizes]
-# accuracies = np.max(accuracies, axis=1)
-# top = accuracies[:, 0]
-# imax = np.argmax(top)
-# print("The best accuracy=%.4f at (bound, batchSize)=(%f, %d)" % (top[imax], bounds[x[imax]], batchSizes[y[imax]]))
-# top -= OffsetAcc
-# bottom = np.zeros_like(top)
-
-# fig = plt.figure(figsize=(8, 8))
-# ax2 = fig.add_subplot(111, projection='3d')
-# ax2.bar3d(x, y, bottom, widthBound, widthBatchSize, top, shade=True, color='w')
-# ax2.xaxis.set_major_formatter(mticker.FuncFormatter(bounds_tick_formatter))
-# ax2.yaxis.set_major_formatter(mticker.FuncFormatter(batchSize_tick_formatter))
-# ax2.zaxis.set_major_formatter(mticker.FuncFormatter(accuracy_tick_formatter))
-# ax2.set_xticks(_x)
-# ax2.set_yticks(_y)
-# ax2.set_xlabel("bound")
-# ax2.set_ylabel("batch size")
-# ax2.set_zlabel("Accuracy")
-# ax2.set_zlim(ZLimMin, ZLimMax)
-# ax2.set_title('(bound, batch size) vs. Accuracy')
-# ###
-# _x = np.arange(len(epsilons))
-# _y = np.arange(len(batchSizes))
-# _xx, _yy = np.meshgrid(_x, _y)
-# x, y = _xx.ravel(), _yy.ravel()
-# iEpsilons = [np.where(Epsilons == epsilons[x[i]])[0][0] for i in range(len(x))]
-# iBatchSizes = [np.where(BatchSizes == batchSizes[y[i]])[0][0] for i in range(len(y))]
-# accuracies = results[:, iEpsilons, iBatchSizes]
-# accuracies = np.max(accuracies, axis=0)
-# top = accuracies[:, 0]
-# imax = np.argmax(top)
-# print("The best accuracy=%.4f at (epsilon, batchSize)=(%f, %d)" % (top[imax], epsilons[x[imax]], batchSizes[y[imax]]))
-# top -= OffsetAcc
-# bottom = np.zeros_like(top)
-
-# fig = plt.figure(figsize=(9, 8))
-# ax3 = fig.add_subplot(111, projection='3d')
-# ax3.bar3d(x, y, bottom, widthEpsilon, widthBatchSize, top, shade=True, color='w')
-# ax3.xaxis.set_major_formatter(mticker.FuncFormatter(epsilon_tick_formatter))
-# ax3.yaxis.set_major_formatter(mticker.FuncFormatter(batchSize_tick_formatter))
-# ax3.zaxis.set_major_formatter(mticker.FuncFormatter(accuracy_tick_formatter))
-# ax3.set_xticks(_x)
-# ax3.set_yticks(_y)
-# ax3.set_xlabel("$\epsilon$")
-# ax3.set_ylabel("batch size")
-# ax3.set_zlabel("Accuracy")
-# ax3.set_zlim(ZLimMin, ZLimMax)
-# ax3.set_title('($\epsilon$, batch size) vs. Accuracy')
-
-# plt.show()
